98dceff74272c8214f51538f88e3e63450607fd6
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-08-23 00:25:32 +0100

    3.5.0.0

--- Update/Files/test.bat
+++ Update/Files/test.bat
@@ -1,7 +1,7 @@
-@ECHO OFF
 CD C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs
 md "Steam Deck Launcher"
 CD C:\"Steam Deck Launcher"
 copy "SDL Website.url" C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\"Steam Deck Launcher"
 copy "GitHub Wiki.url" C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\"Steam Deck Launcher"
 copy "SDL - GitHub.url" C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\"Steam Deck Launcher"
+Pause
