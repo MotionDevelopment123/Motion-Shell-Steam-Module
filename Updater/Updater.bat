7f8088c6996fdcd580874ca985ab041562d09458
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-08-22 23:03:39 +0100

    Updater improvements

--- Updater/Updater.bat
+++ Updater/Updater.bat
@@ -13,7 +13,11 @@ taskkill /f /im SDL.exe /T
 taskkill /f /im steam.exe /T
 CD C:\"Steam Deck Launcher"\Updater\Files
 curl.exe -o Update.zip https://raw.githubusercontent.com/MotionDevelopment123/SDL/STABLE-VERSION/Update/Files/update.zip
+curl.exe -o Update.bat https://raw.githubusercontent.com/MotionDevelopment123/SDL/STABLE-VERSION/Update/Files/update.bat
+@ECHO Donwload complete, extracting files...
+@ECHO OFF
 tar -xf Update.zip --directory c:\"Steam Deck Launcher"\
+CALL Update.bat
 @ECHO Version %LVERSION% installed
 @ECHO OFF
 timeout /t 5
