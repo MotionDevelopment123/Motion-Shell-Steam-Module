81f6c3b0e570dfa04e3ff1dabc59c3c5194a6f5a
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-08-26 22:22:30 +0100

    3.5.1.0

--- SDL/SDL.bat
+++ SDL/SDL.bat
@@ -1,12 +1,10 @@
 @ECHO OFF
-CD C:\"Steam Deck Launcher"\Updater
-START Updater.BAT
+CD C:\"Steam Deck Launcher"\Updater\SCRIPTS
+START Updater.vbs
 CD C:\"Steam Deck Launcher"
 set /p STEAMLOCATION=<STEAM.LOCATION
 START /WAIT %STEAMLOCATION% -noverifyfiles -gamepadui
 set /p CLOSEDLOCATION=<CLOSED.LOCATION
 START %CLOSEDLOCATION%
-CD C:\"Steam Deck Launcher"\Updater
-START Updater.BAT
 
 EXIT
